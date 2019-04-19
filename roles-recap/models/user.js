36098edcab6a86b09013b5a77eb8808102181f6c
mjarraya <mjarraya@student.42.fr>
2019-04-19 16:31:38 +0200

    roles recap

---
+++ roles-recap/models/user.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  username: {
+    type: String
+  },
+  password: {
+    type: String
+  },
+  githubId: {
+    type: String
+  },
+  facebookId: {
+    type: String
+  },
+  displayName: {
+    type: String
+  },
+  role: {
+    type: String,
+    enum: ['guest', 'mod', 'admin'],
+    default: 'guest'
+  }
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
---
+++ roles-recap/routes/protected.js
@@ -0,0 +1,82 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/user');
+const Room = require('../models/room');
+
+const authenticationCheck = (req, res, next) => {
+  if (req.isAuthenticated()) next();
+  else res.render('error', { errorMessage: 'This is a protected route' });
+};
+
+const rolesCheck = role => {
+  return (req, res, next) => {
+    if (!req.isAuthenticated()) {
+      res.render('error', { errorMessage: 'This is a protected route' });
+    } else if (req.user.role !== role) {
+      res.render('error', {
+        errorMessage: 'You do not have sufficient privileges'
+      });
+    } else {
+      next();
+    }
+  };
+};
+
+router.get('/secret', authenticationCheck, (req, res, next) => {
+  res.render('protected/secret');
+});
+
+router.get('/rooms', authenticationCheck, (req, res, next) => {
+  //   let queryObj = {};
+  //   if (req.user.role !== 'admin') queryObj.owner = req.user._id;
+  //   Room.find(queryObj);
+  Room.find({ owner: req.user._id })
+    .then(rooms => {
+      res.render('protected/rooms', { rooms });
+    })
+    .catch(err => {
+      console.error('Error while getting the rooms', err);
+    });
+});
+
+router.get('/roomsAll', rolesCheck('mod'), (req, res, next) => {
+  Room.find({})
+    .then(rooms => {
+      res.render('protected/rooms', { rooms });
+    })
+    .catch(err => {
+      console.error('Error while getting the rooms', err);
+    });
+});
+
+router.get('/rooms/add', authenticationCheck, (req, res, next) => {
+  res.render('protected/room-create');
+});
+
+router.post('/rooms/add', authenticationCheck, (req, res, next) => {
+  const { location, price, description } = req.body;
+  console.log(req.user._id);
+  Room.create({ location, price, description, owner: req.user._id })
+    .then(room => {
+      res.redirect('/rooms');
+    })
+    .catch(err => {
+      console.error('Error while adding a new room', err);
+    });
+});
+
+router.get('/users/edit', rolesCheck('admin'), (req, res, next) => {
+  User.find({ role: { $ne: 'admin' } }).then(users => {
+    res.render('protected/users-edit', { users });
+  });
+});
+
+router.post('/users/edit/:userId', rolesCheck('admin'), (req, res, next) => {
+  User.updateOne({ _id: req.params.userId }, { role: req.body.role }).then(
+    () => {
+      res.redirect('/users/edit');
+    }
+  );
+});
+
+module.exports = router;
